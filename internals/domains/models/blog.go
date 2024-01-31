08acd2e10c7466a29f8600ca79948a657e14bd43
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2024-01-31 16:22:28 +0700

    feat: add example in model :money_with_wings:

--- internals/domains/models/blog.go
+++ internals/domains/models/blog.go
@@ -2,10 +2,10 @@ package models
 
 type BlogModel struct {
 	CommonColumnModel
-	AccountID   uint    `gorm:"column:account_id;type:int" json:"account_id"`
-	Name        string  `gorm:"column:name;type:VARCHAR(255);unique;not null" json:"name" validate:"required"`
-	Description *string `gorm:"column:description;type:TEXT;" json:"description"`
-	Active      *bool   `gorm:"column:is_active;default:true" json:"active"`
+	AccountID   uint    `gorm:"column:account_id;type:int" json:"account_id" example:"1"`
+	Name        string  `gorm:"column:name;type:VARCHAR(255);unique;not null" json:"name" validate:"required" example:"blog"`
+	Description *string `gorm:"column:description;type:TEXT;" json:"description" example:"blog description"`
+	Active      *bool   `gorm:"column:is_active;default:true" json:"active" example:"true"`
 }
 
 func (BlogModel) TableName() string {
